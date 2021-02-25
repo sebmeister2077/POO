bdeb6a470b1bfe8b0a28b001cead8d1319e19b83
sebask <sebaskolster@gmail.com>
2021-02-25 16:17:50 +0200

    added complex operations

--- Complex/Program.cs
+++ Complex/Program.cs
@@ -12,11 +12,8 @@ namespace Complex
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                Complex c = new Complex(Console.ReadLine());
-                Console.WriteLine(c.ToString());
-            }
+            Console.WriteLine(new Complex("+8-3i")/new Complex("i"));
+            Console.ReadLine();
         }
     }
     class Complex
@@ -29,19 +26,18 @@ namespace Complex
             try
             {
                 string[] parts = new string[2];
-                for (int i = 0; i < str.Length; i++)
-                    if (i>0 && (str[i] == '-' || str[i] == '+'))
-                    {
-                        parts[0] = str.Substring(0, i);
-                        parts[1] = str.Substring(i, str.Length - i);
-                        break;
-                    }
-                if(parts[1]=="")//exista doar r sau doar i
+                int i;
+                for (i = 1; i < str.Length; i++)
+                    if(str[i] == '-' || str[i] == '+') 
+                       break;
+                parts[0] = str.Substring(0, i);
+                parts[1] = str.Substring(i, str.Length - i);
+                if (parts[1]=="")//exista doar r sau doar i
                 {
                     if(parts[0][parts[0].Length-1]=='i')
                     {
                         re = 0;
-                        im = double.Parse(parts[0].Substring(0, parts[0].Length - 1));
+                        im = ParseImag(parts[0]);
                     }
                     else
                     {
@@ -52,11 +48,28 @@ namespace Complex
                 else
                 {
                     re = double.Parse(parts[0]);
-                    im = double.Parse(parts[1].Substring(0, parts[1].Length - 1));
+                    im = ParseImag(parts[1]);
                 }
                         
             }
             catch { Console.WriteLine("Error parsing string parameter to Complex"); }
+        }
+        private double ParseImag(string str)
+        {
+            bool ok = false;//presupunem ca avem ...+i sau ...-i sau i
+            double n;
+            for (int i = 0; i < 2 && i < str.Length; i++)
+                if (str[i] >= '0' && str[i] <= '9')
+                    ok = true;
+            if (!ok)
+            {
+                n = 1;// +i sau i = 1i
+                if (str[0] == '-')
+                    n= -n;
+            }
+            else
+                n= double.Parse(str.Substring(0, str.Length - 1));
+            return n;
         }
         public Complex(double r, double i=0)
         {
@@ -70,9 +83,10 @@ namespace Complex
         public static Complex operator *(Complex c1, Complex c2) =>
             new Complex(c1.re*c2.re-c1.im*c2.im, c1.re*c2.im + c1.im*c2.re);
         public static Complex operator ^(Complex c, int putere) =>
-            Putere(c,putere);
-
-        public static Complex Putere(Complex c, int putere)
+            Power(c,putere);
+        public static Complex operator /(Complex c1, Complex c2) =>
+            Divide(c1, c2);
+        public static Complex Power(Complex c, int putere)
         {
             Complex aux = c;
             putere--;
@@ -80,7 +94,7 @@ namespace Complex
                 c = c * aux;
             return c;
         }
-        public static Complex Multiplication(Complex c1,Complex c2)
+        public static Complex Multiply(Complex c1,Complex c2)
         {
             return new Complex(c1.re * c2.re - c1.im * c2.im, c1.re * c2.im + c1.im * c2.re);
         }
@@ -91,6 +105,17 @@ namespace Complex
         public static Complex Add(Complex c1,Complex c2)
         {
             return new Complex(c1.re +c2.re, c1.im + c2.im);
+        }
+        public static Complex Divide(Complex c1,Complex c2)
+        {
+            //inmultesti cu conjugata/conjugata
+            Complex aux1 = c1 * Conjugate(c2);
+            double aux2 = (c2 * Conjugate(c2)).re;//devine real
+            return new Complex(aux1.re / aux2, aux1.im / aux2);
+        }
+        public static Complex Conjugate(Complex c)
+        {
+            return new Complex(c.re, -c.im);
         }
 
         public override string ToString()
